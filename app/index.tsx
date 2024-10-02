d2f85f8e4c786a1d6d479ca7fc5ffa7c47e5cfd3
mayank2130 <mayankthakur1712@gmail.com>
2024-10-02 12:40:45 +0530

    redirect to home if signed in

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Redirect } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Home = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
   return <Redirect href="/(auth)/welcome" />;
 };
 
